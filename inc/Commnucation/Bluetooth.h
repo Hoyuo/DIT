fa29e0b11c456acbc2aca7bb245d2c177e314a3b
ParkBeomChan <zeroise91@gmail.com>
2015-07-02 09:10:32 +0900

    bluetooth recv completed

--- inc/Commnucation/Bluetooth.h
+++ inc/Commnucation/Bluetooth.h
@@ -19,7 +19,7 @@ typedef struct _Bluetooth {
 
     bool (* onBluetoothDisconnect)(Bluetooth* this_gen);
 
-    int (* BluetoothRecv)(Bluetooth* this_gen, char* recvbuffer);
+    int (* BluetoothRecv)(Bluetooth* this_gen);
 
     int (* BluetoothSend)(Bluetooth* this_gen, char* sendbuffer);
 
@@ -31,28 +31,33 @@ typedef struct _Bluetooth {
 
     GList *searched_list;
     GList *bound_list;
+    char* remoteMACAddr;
+
 } Bluetooth;
 
 Bluetooth* NewBluetooth();
 
 void DestroyBluetooth(Bluetooth* this_gen);
 
-bool isBluetoothAccessible(Bluetooth* this_gen);
+bool isBTAccessible(Bluetooth* this_gen);
 
-bool onBluetoothConnect(Bluetooth* this_gen);
+bool onBTConnect(Bluetooth* this_gen);
 
-bool onBluetoothDisconnect(Bluetooth* this_gen);
+bool onBTDisconnect(Bluetooth* this_gen);
 
-int BluetoothRecv(Bluetooth* this_gen, char* recvbuffer);
+int BTRecv(Bluetooth* this_gen);
 
-int BluetoothSend(Bluetooth* this_gen, char* sendbuffer);
+int BTSend(Bluetooth* this_gen, char* sendbuffer);
 
-bool isBluetoothConnected(Bluetooth* this_gen);
+bool isBTConnected(Bluetooth* this_gen);
+
+GList* BTSearch(Bluetooth* this_gen);
+
+GList* BTBound(Bluetooth* this_gen);
 
 typedef struct {
     Bluetooth Bluetooth;
 
-    char* remoteMACAddr;
     bool connected;
     bool accessible;
 } BluetoothExtends;
