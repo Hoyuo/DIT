9092e78c5bf060be6112bdc5eb7814d0b8487fe2
DongJin <piaf@sogang.ac.kr>
2015-07-07 12:09:49 +0900

    Sensor init

--- inc/Device/Sensor.h
+++ inc/Device/Sensor.h
@@ -1,12 +1,24 @@
 #ifndef DIT_SENSOR_H
 #define DIT_SENSOR_H
 
+#include "dit.h"
 #include <stdbool.h>
 #include <stdalign.h>
+#include <sensor.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct _Sensor Sensor;
+typedef struct _Sensor {
+
+}Sensor;
+
+
+typedef struct _SensorExtend{
+
+} SensorExtend;
 
 
 #ifdef __cplusplus
--- src/Device/Sensor.c
+++ src/Device/Sensor.c
@@ -1,2 +1,3 @@
 #include "Device/Sensor.h"
 #include <stdlib.h>
+#include <sensor.h>
