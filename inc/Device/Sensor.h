7b74e1f93733b53b938ef1e3d226d86a18adcb9d
DongJin <piaf@sogang.ac.kr>
2015-07-07 15:12:21 +0900

    Init Sensor api

--- inc/Device/Sensor.h
+++ inc/Device/Sensor.h
@@ -15,30 +15,56 @@ typedef struct _Sensor {
 
 }Sensor;
 
-sensor_event_s getAllSensorData();
-bool hasBarometer();
-sensor_event_s getBarometerData();
-bool hasMagnetoMeter();
-sensor_event_s getMagnetoMeterData();
-bool hasPhotoMeter();
-sensor_event_s getPhotoMeterData();
-bool hasProximity();
-sensor_event_s getProximityData();
-bool hasTiltMeter();
-sensor_event_s getTiltMeterData();
-bool hasTemperature();
-sensor_event_s getTemperatureData();
-bool hasUltraViolet();
-sensor_event_s getUltraVioletData();
-bool hasHumidity();
-sensor_event_s getHumidityData();
-bool hasLinearAccelation();
-sensor_event_s getLinearAccelation();
-bool hasRotationVector();
-sensor_event_s getRotationVectorData();
-bool hasGravity();
-sensor_event_s getGravityData();
 
+Sensor* NewSensor();
+
+void DestroySensor(Sensor* this_gen);
+
+sensor_event_s getAllSensorData(Sensor* this_gen);
+
+bool hasBarometer(Sensor* this_gen);
+
+sensor_event_s getBarometerData(Sensor* this_gen);
+
+bool hasMagnetoMeter(Sensor* this_gen);
+
+sensor_event_s getMagnetoMeterData(Sensor* this_gen);
+
+bool hasPhotoMeter(Sensor* this_gen);
+
+sensor_event_s getPhotoMeterData(Sensor* this_gen);
+
+bool hasProximity(Sensor* this_gen);
+
+sensor_event_s getProximityData(Sensor* this_gen);
+
+bool hasTiltMeter(Sensor* this_gen);
+
+sensor_event_s getTiltMeterData(Sensor* this_gen);
+
+bool hasTemperature(Sensor* this_gen);
+
+sensor_event_s getTemperatureData(Sensor* this_gen);
+
+bool hasUltraViolet(Sensor* this_gen);
+
+sensor_event_s getUltraVioletData(Sensor* this_gen);
+
+bool hasHumidity(Sensor* this_gen);
+
+sensor_event_s getHumidityData(Sensor* this_gen);
+
+bool hasLinearAccelation(Sensor* this_gen);
+
+sensor_event_s getLinearAccelation(Sensor* this_gen);
+
+bool hasRotationVector(Sensor* this_gen);
+
+sensor_event_s getRotationVectorData(Sensor* this_gen);
+
+bool hasGravity(Sensor* this_gen);
+
+sensor_event_s getGravityData(Sensor* this_gen);
 
 typedef struct _SensorExtend{
 
