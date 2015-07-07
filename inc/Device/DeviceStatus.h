4e29066d5fe4b23f8c819b9acb44b5fda432f1be
DongJin <piaf@sogang.ac.kr>
2015-07-07 15:06:17 +0900

    modify indents

--- inc/Device/DeviceStatus.h
+++ inc/Device/DeviceStatus.h
@@ -1,7 +1,6 @@
 #ifndef DIT_VIBRATOR_H
 #define DIT_VIBRATOR_H
 
-
 #include "dit.h"
 
 #include <stdbool.h>
@@ -19,27 +18,44 @@ extern "C" {
 typedef struct _DeviceStatus DeviceStatus;
 typedef struct _DeviceStatus {
 	int (* getBattery)(DeviceStatus* this_gen);
+
 	bool (* isCharging)(DeviceStatus* this_gen);
-    void (* DisplayLock)(DeviceStatus* this_gen);
-    void (* DisplayUnlock)(DeviceStatus* this_gen);
-    void (* DisplayDim)(DeviceStatus* this_gen);
-    void (* FlashOn)(DeviceStatus* this_gen);
-    void (* FlashOff)(DeviceStatus* this_gen);
-    int (* getDisplay)(DeviceStatus* this_gen);
-    void (* setDisplay)(DeviceStatus* this_gen, int bright);
+
+	void (* DisplayLock)(DeviceStatus* this_gen);
+
+	void (* DisplayUnlock)(DeviceStatus* this_gen);
+
+	void (* DisplayDim)(DeviceStatus* this_gen);
+
+	void (* FlashOn)(DeviceStatus* this_gen);
+
+	void (* FlashOff)(DeviceStatus* this_gen);
+
+	int (* getDisplay)(DeviceStatus* this_gen);
+
+	void (* setDisplay)(DeviceStatus* this_gen, int bright);
 }DeviceStatus;
 
 DeviceStatus* NewDeviceStatus();
+
 void DestroyDeviceStatus(DeviceStatus* this_gen);
 
 int getBatteryRemainsPercent(DeviceStatus* this_gen);
+
 bool isBatteryCharging(DeviceStatus* this_gen);
+
 void DisplayLock(DeviceStatus* this_gen);
+
 void DisplayUnlock(DeviceStatus* this_gen);
+
 void DisplayDim(DeviceStatus* this_gen);
+
 void onFlash(DeviceStatus* this_gen);
+
 void offFlash(DeviceStatus* this_gen);
+
 int getDisplayBrightLevel(DeviceStatus* this_gen);
+
 void setDisplayBrightLevel(DeviceStatus* this_gen, int bright);
 
 typedef struct _DeviceStatusExtend{
--- inc/Device/Sensor.h
+++ inc/Device/Sensor.h
@@ -15,6 +15,30 @@ typedef struct _Sensor {
 
 }Sensor;
 
+sensor_event_s getAllSensorData();
+bool hasBarometer();
+sensor_event_s getBarometerData();
+bool hasMagnetoMeter();
+sensor_event_s getMagnetoMeterData();
+bool hasPhotoMeter();
+sensor_event_s getPhotoMeterData();
+bool hasProximity();
+sensor_event_s getProximityData();
+bool hasTiltMeter();
+sensor_event_s getTiltMeterData();
+bool hasTemperature();
+sensor_event_s getTemperatureData();
+bool hasUltraViolet();
+sensor_event_s getUltraVioletData();
+bool hasHumidity();
+sensor_event_s getHumidityData();
+bool hasLinearAccelation();
+sensor_event_s getLinearAccelation();
+bool hasRotationVector();
+sensor_event_s getRotationVectorData();
+bool hasGravity();
+sensor_event_s getGravityData();
+
 
 typedef struct _SensorExtend{
 
