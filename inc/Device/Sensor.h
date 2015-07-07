1f1040404ff71b521b52b39d710e5624eb0c3837
DongJin <piaf@sogang.ac.kr>
2015-07-07 15:37:49 +0900

    Sensor api function init

--- inc/Device/Sensor.h
+++ inc/Device/Sensor.h
@@ -10,8 +10,57 @@
 extern "C" {
 #endif
 
+typedef struct _SensorValue {
+	float values[MAX_VALUE_SIZE];
+}SensorValue;
+
 typedef struct _Sensor Sensor;
 typedef struct _Sensor {
+	SensorValue (* getAllSensorData)(Sensor* this_gen);
+
+	bool (* hasBarometer)(Sensor* this_gen);
+
+	SensorValue (* getBarometerData)(Sensor* this_gen);
+
+	bool (* hasMagnetoMeter)(Sensor* this_gen);
+
+	SensorValue (* getMagnetoMeterData)(Sensor* this_gen);
+
+	bool (* hasPhotoMeter)(Sensor* this_gen);
+
+	SensorValue (* getPhotoMeterData)(Sensor* this_gen);
+
+	bool (* hasProximity)(Sensor* this_gen);
+
+	SensorValue (* getProximityData)(Sensor* this_gen);
+
+	bool (* hasTiltMeter)(Sensor* this_gen);
+
+	SensorValue (* getTiltMeterData)(Sensor* this_gen);
+
+	bool (* hasTemperature)(Sensor* this_gen);
+
+	SensorValue (* getTemperatureData)(Sensor* this_gen);
+
+	bool (* hasUltraViolet)(Sensor* this_gen);
+
+	SensorValue (* getUltraVioletData)(Sensor* this_gen);
+
+	bool (* hasHumidity)(Sensor* this_gen);
+
+	SensorValue (* getHumidityData)(Sensor* this_gen);
+
+	bool (* hasLinearAccelation)(Sensor* this_gen);
+
+	SensorValue (* getLinearAccelation)(Sensor* this_gen);
+
+	bool (* hasRotationVector)(Sensor* this_gen);
+
+	SensorValue (* getRotationVectorData)(Sensor* this_gen);
+
+	bool (* hasGravity)(Sensor* this_gen);
+
+	SensorValue (* getGravityData)(Sensor* this_gen);
 
 }Sensor;
 
@@ -20,54 +69,54 @@ Sensor* NewSensor();
 
 void DestroySensor(Sensor* this_gen);
 
-sensor_event_s getAllSensorData(Sensor* this_gen);
+SensorValue getAllSensorData(Sensor* this_gen);
 
 bool hasBarometer(Sensor* this_gen);
 
-sensor_event_s getBarometerData(Sensor* this_gen);
+SensorValue getBarometerData(Sensor* this_gen);
 
 bool hasMagnetoMeter(Sensor* this_gen);
 
-sensor_event_s getMagnetoMeterData(Sensor* this_gen);
+SensorValue getMagnetoMeterData(Sensor* this_gen);
 
 bool hasPhotoMeter(Sensor* this_gen);
 
-sensor_event_s getPhotoMeterData(Sensor* this_gen);
+SensorValue getPhotoMeterData(Sensor* this_gen);
 
 bool hasProximity(Sensor* this_gen);
 
-sensor_event_s getProximityData(Sensor* this_gen);
+SensorValue getProximityData(Sensor* this_gen);
 
 bool hasTiltMeter(Sensor* this_gen);
 
-sensor_event_s getTiltMeterData(Sensor* this_gen);
+SensorValue getTiltMeterData(Sensor* this_gen);
 
 bool hasTemperature(Sensor* this_gen);
 
-sensor_event_s getTemperatureData(Sensor* this_gen);
+SensorValue getTemperatureData(Sensor* this_gen);
 
 bool hasUltraViolet(Sensor* this_gen);
 
-sensor_event_s getUltraVioletData(Sensor* this_gen);
+SensorValue getUltraVioletData(Sensor* this_gen);
 
 bool hasHumidity(Sensor* this_gen);
 
-sensor_event_s getHumidityData(Sensor* this_gen);
+SensorValue getHumidityData(Sensor* this_gen);
 
 bool hasLinearAccelation(Sensor* this_gen);
 
-sensor_event_s getLinearAccelation(Sensor* this_gen);
+SensorValue getLinearAccelation(Sensor* this_gen);
 
 bool hasRotationVector(Sensor* this_gen);
 
-sensor_event_s getRotationVectorData(Sensor* this_gen);
+SensorValue getRotationVectorData(Sensor* this_gen);
 
 bool hasGravity(Sensor* this_gen);
 
-sensor_event_s getGravityData(Sensor* this_gen);
+SensorValue getGravityData(Sensor* this_gen);
 
 typedef struct _SensorExtend{
-
+	Sensor sensor;
 } SensorExtend;
 
 
--- src/Device/Sensor.c
+++ src/Device/Sensor.c
@@ -1,3 +1,153 @@
 #include "Device/Sensor.h"
 #include <stdlib.h>
 #include <sensor.h>
+
+Sensor* NewSensor()
+{
+	SensorExtend* this = (SensorExtend*)malloc(sizeof(SensorExtend));
+	this->sensor.getAllSensorData = getAllSensorData;
+	this->sensor.hasBarometer = hasBarometer;
+	this->sensor.getBarometerData = getBarometerData;
+	this->sensor.hasMagnetoMeter = hasMagnetoMeter;
+	this->sensor.getMagnetoMeterData = getMagnetoMeterData;
+	this->sensor.hasPhotoMeter = hasPhotoMeter;
+	this->sensor.getPhotoMeterData = getPhotoMeterData;
+	this->sensor.hasProximity = hasProximity;
+	this->sensor.getProximityData = getProximityData;
+	this->sensor.hasTiltMeter = hasTiltMeter;
+	this->sensor.getTiltMeterData = getTiltMeterData;
+	this->sensor.hasTemperature = hasTemperature;
+	this->sensor.getTemperatureData = getTemperatureData;
+	this->sensor.hasUltraViolet = hasUltraViolet;
+	this->sensor.getUltraVioletData = getUltraVioletData;
+	this->sensor.hasHumidity = hasHumidity;
+	this->sensor.getHumidityData = getHumidityData;
+	this->sensor.hasLinearAccelation = hasLinearAccelation;
+	this->sensor.getLinearAccelation = getLinearAccelation;
+	this->sensor.hasRotationVector = hasRotationVector;
+	this->sensor.getRotationVectorData = getRotationVectorData;
+	this->sensor.hasGravity = hasGravity;
+	this->sensor.getGravityData = getGravityData;
+
+	return this->sensor;
+}
+
+void DestroySensor(Sensor* this_gen)
+{
+
+}
+
+SensorValue getAllSensorData(Sensor* this_gen)
+{
+
+}
+
+bool hasBarometer(Sensor* this_gen)
+{
+
+}
+
+SensorValue getBarometerData(Sensor* this_gen)
+{
+
+}
+
+bool hasMagnetoMeter(Sensor* this_gen)
+{
+
+}
+
+SensorValue getMagnetoMeterData(Sensor* this_gen)
+{
+
+}
+
+bool hasPhotoMeter(Sensor* this_gen)
+{
+
+}
+
+SensorValue getPhotoMeterData(Sensor* this_gen)
+{
+
+}
+
+bool hasProximity(Sensor* this_gen)
+{
+
+}
+
+SensorValue getProximityData(Sensor* this_gen)
+{
+
+}
+
+bool hasTiltMeter(Sensor* this_gen)
+{
+
+}
+
+SensorValue getTiltMeterData(Sensor* this_gen)
+{
+
+}
+
+bool hasTemperature(Sensor* this_gen)
+{
+
+}
+
+SensorValue getTemperatureData(Sensor* this_gen)
+{
+
+}
+
+bool hasUltraViolet(Sensor* this_gen)
+{
+
+}
+
+SensorValue getUltraVioletData(Sensor* this_gen)
+{
+
+}
+
+bool hasHumidity(Sensor* this_gen)
+{
+
+}
+
+SensorValue getHumidityData(Sensor* this_gen)
+{
+
+}
+
+bool hasLinearAccelation(Sensor* this_gen)
+{
+
+}
+
+SensorValue getLinearAccelation(Sensor* this_gen)
+{
+
+}
+
+bool hasRotationVector(Sensor* this_gen)
+{
+
+}
+
+SensorValue getRotationVectorData(Sensor* this_gen)
+{
+
+}
+
+bool hasGravity(Sensor* this_gen)
+{
+
+}
+
+SensorValue getGravityData(Sensor* this_gen)
+{
+
+}
