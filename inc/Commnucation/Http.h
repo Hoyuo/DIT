9ac9123b3796040af012500eb116b97eb6884f69
Hoyuo <psh9907@gmail.com>
2015-07-20 05:35:51 +0900

    Refactoring #7 HTTP Changed

--- inc/Commnucation/Http.h
+++ inc/Commnucation/Http.h
@@ -4,59 +4,59 @@
 #include <stdbool.h>
 #include <stdalign.h>
 #include <stddef.h>
+
+#include "dit.h"
+
 #include <curl.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef struct _Http Http;
+typedef struct _Http * Http;
 
-typedef struct _Http {
-	bool (*isAccessible)(Http* this_gen);
+struct _Http
+{
+    bool (* isAccessible) (Http this_gen);
 
-	bool (*onConnect)(Http* this_gen, char* url, int port);
+    bool (* onConnect) (Http this_gen, String url, int port);
 
-	bool (*onDisconnect)(Http* this_gen);
+    bool (* onDisconnect) (Http this_gen);
 
-	void (*Download)(Http* this_gen, char* filename);
+    void (* Download) (Http this_gen, String filename);
 
-	void (*Post)(Http* this_gen, char* res, char** req);
+    void (* Post) (Http this_gen, String res, String * req);
 
-	void (*Get)(Http* this_gen, char* res, char** req);
+    void (* Get) (Http this_gen, String res, String * req);
 
-} Http;
+};
 
 /*
  * @privlevel public
  * @privilege %http://tizen.org/privilege/internet
  */
-Http* NewHttp();
+Http NewHttp (void);
+void DestoryHttp (Http this_gen);
+bool isHttpAccessible (Http this_gen);
+bool onHttpConnect (Http this_gen, String url, int port);
+bool onHttpDisconnect (Http this_gen);
+void HttpDownload (Http this_gen, String filename);
+void HttpExcutePost (Http this_gen, String req, String * res);
+void HttpExcuteGet (Http this_gen, String req, String * res);
 
-void DestoryHttp(Http* this_gen);
+static size_t write_callback (void * contents, size_t size, size_t nmemb, void ** res);
+static size_t write_data (void * ptr, size_t size, size_t nmemb, FILE * stream);
 
-bool isHttpAccessible(Http* this_gen);
+const char * HttpErrorCheck (CURLcode errorCode);
 
-bool onHttpConnect(Http* this_gen, char* url, int port);
+typedef struct
+{
+    struct _Http http;
+    String       url;
+    int          port;
+    bool         access;
+    bool         conect;
 
-bool onHttpDisconnect(Http* this_gen);
-
-void HttpDownload(Http* this_gen, char* filename);
-
-void HttpExcutePost(Http* this_gen, char* req, char** res);
-
-void HttpExcuteGet(Http* this_gen, char* req, char** res);
-
-static size_t write_callback(void* contents, size_t size, size_t nmemb, void** res);
-
-static size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream);
-
-typedef struct {
-	Http http;
-	char* url;
-	int port;
-	bool access;
-	bool conect;
 } HttpExtends;
 
 #ifdef __cplusplus
--- src/Commnucation/Http.c
+++ src/Commnucation/Http.c
@@ -1,7 +1,9 @@
 #include "Commnucation/Http.h"
+
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <curl.h>
 #include <system_info.h>
 
@@ -10,97 +12,103 @@
  * @privilege %http://tizen.org/privilege/internet
  */
 
-Http* NewHttp() {
-	HttpExtends* this = (HttpExtends*) malloc(sizeof(HttpExtends));
+Http NewHttp (void)
+{
+    HttpExtends * this = (HttpExtends *)malloc (sizeof (HttpExtends));
 
-	this->http.isAccessible = isHttpAccessible;
-	this->http.onConnect = onHttpConnect;
-	this->http.onDisconnect = onHttpDisconnect;
-	this->http.Download = HttpDownload;
-	this->http.Get = HttpExcuteGet;
-	this->http.Post = HttpExcutePost;
+    this->http.isAccessible = isHttpAccessible;
+    this->http.onConnect    = onHttpConnect;
+    this->http.onDisconnect = onHttpDisconnect;
+    this->http.Download     = HttpDownload;
+    this->http.Get          = HttpExcuteGet;
+    this->http.Post         = HttpExcutePost;
 
-	this->url = NULL;
-	this->port = 80;
+    this->url  = NULL;
+    this->port = 80;
 
-	this->access = false;
-	this->conect = false;
+    this->access = false;
+    this->conect = false;
 
-	return &this->http;
+    return &this->http;
 }
 
-void DestoryHttp(Http* this_gen) {
-	if (this_gen != NULL) {
-		HttpExtends* this = (HttpExtends*) this_gen;
+void DestoryHttp (Http this_gen)
+{
+    if ( this_gen != NULL)
+    {
+        HttpExtends * this = (HttpExtends *)this_gen;
 
-		if (this->url != NULL)
-			free(this->url);
+        if ( this->url != NULL)
+        {
+            free (this->url);
+        }
 
-		free(this);
-	}
+        free (this);
+    }
 }
 
-bool isHttpAccessible(Http* this_gen) {
-	HttpExtends* this = (HttpExtends*) this_gen;
-	bool check1, check2;
+bool isHttpAccessible (Http this_gen)
+{
+    HttpExtends * this = (HttpExtends *)this_gen;
+    bool check1, check2;
 
-	system_info_get_platform_bool("http://tizen.org/feature/network.wifi",
-			&check1);
-	system_info_get_platform_bool("http://tizen.org/feature/network.telephony",
-			&check2);
+    system_info_get_platform_bool ("http://tizen.org/feature/network.wifi", &check1);
+    system_info_get_platform_bool ("http://tizen.org/feature/network.telephony", &check2);
 
-	this->access = check1 || check2;
-	return this->access;
+    this->access = check1 || check2;
+    return this->access;
 }
 
-bool onHttpConnect(Http* this_gen, char* url, int port) {
-	HttpExtends* this = (HttpExtends*) this_gen;
+bool onHttpConnect (Http this_gen, String url, int port)
+{
+    HttpExtends * this = (HttpExtends *)this_gen;
 
-	if (this->access) {
-		int lenght = strlen(url);
-		this->url = (char *) malloc(lenght + 1);
-		strcpy(this->url, url);
-		this->port = port;
+    if ( this->access )
+    {
+        int lenght = strlen (url);
+        this->url = (String)malloc (lenght + 1);
+        strcpy(this->url, url);
+        this->port = port;
 
-		CURL* curl;
-		CURLcode r;
-		curl_global_init(CURL_GLOBAL_ALL);
+        CURL * curl;
+        CURLcode r;
+        curl_global_init (CURL_GLOBAL_ALL);
 
-		curl = curl_easy_init();
-		if (curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, url);
-			curl_easy_setopt(curl, CURLOPT_PORT, port);
+        curl = curl_easy_init ();
+        if ( curl )
+        {
+            curl_easy_setopt (curl, CURLOPT_URL, url);
+            curl_easy_setopt (curl, CURLOPT_PORT, port);
 
-			r = curl_easy_perform(curl);
+            r = curl_easy_perform (curl);
 
-			if (r == CURLE_OK) {
-				this->conect = true;
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return false;
-		}
-	} else {
-		return false;
-	}
+            if ( r == CURLE_OK )
+            {
+                this->conect = true;
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
-bool onHttpDisconnect(Http* this_gen) {
-	HttpExtends* this = (HttpExtends*) this_gen;
+bool onHttpDisconnect (Http this_gen)
+{
+    HttpExtends * this = (HttpExtends *)this_gen;
 
-	if (this->access) {
-		if (this->url != NULL) {
-			free(this->url);
-			this->url = NULL;
-		}
+    if ( this->access )
+    {
+        if ( this->url != NULL)
+        {
+            free (this->url);
+            this->url = NULL;
+        }
 
-		this->conect = false;
-		return true;
-	} else {
-		return false;
-	}
+        this->conect = false;
+        return true;
+    }
+    return false;
 }
 
 /*
@@ -108,86 +116,101 @@ bool onHttpDisconnect(Http* this_gen) {
  * @privilege %http://tizne.org/privileges/mediastorage
  * @privilege %http://tizne.org/privileges/download
  */
-void HttpDownload(Http* this_gen, char* filename) {
-	HttpExtends* this = (HttpExtends*) this_gen;
-	if (this->conect) {
-		char* path = (char*) malloc(FILENAME_MAX);
-		strcpy(path, "/opt/usr/media/Downloads/");
-		strcat(path, filename);
+void HttpDownload (Http this_gen, String filename)
+{
+    HttpExtends * this = (HttpExtends *)this_gen;
+    if ( this->conect )
+    {
+        String path = (String)malloc (FILENAME_MAX);
+        strcpy(path, "/opt/usr/media/Downloads/");
+        strcat(path, filename);
 
-		char* url = (char*) malloc(FILENAME_MAX);
-		strcpy(url, this->url);
-		strcat(url, "/");
-		strcat(url, filename);
+        String url = (String)malloc (FILENAME_MAX);
+        strcpy(url, this->url);
+        strcat(url, "/");
+        strcat(url, filename);
 
-		CURL* curl = curl_easy_init();
-		if (curl) {
-			FILE* fp = fopen(path, "wb");
-			curl_easy_setopt(curl, CURLOPT_URL, url);
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-			curl_easy_perform(curl);
-			/* always cleanup */
-			curl_easy_cleanup(curl);
-			fclose(fp);
-		}
-		free(url);
-		free(path);
-	}
+        CURL * curl = curl_easy_init ();
+        if ( curl )
+        {
+            FILE * fp = fopen (path, "wb");
+            curl_easy_setopt (curl, CURLOPT_URL, url);
+            curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_data);
+            curl_easy_setopt (curl, CURLOPT_WRITEDATA, fp);
+            curl_easy_perform (curl);
+            /* always cleanup */
+            curl_easy_cleanup (curl);
+            fclose (fp);
+        }
+        free (url);
+        free (path);
+    }
 }
 
-void HttpExcutePost(Http* this_gen, char* req, char** res) {
-	HttpExtends* this = (HttpExtends*) this_gen;
-	if (this->conect) {
-		CURL* curl;
-		CURLcode r;
+void HttpExcutePost (Http this_gen, String req, String * res)
+{
+    HttpExtends * this = (HttpExtends *)this_gen;
+    if ( this->conect )
+    {
+        CURL * curl;
+        CURLcode r;
 
-		curl = curl_easy_init();
-		if (curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, this->url);
-			curl_easy_setopt(curl, CURLOPT_PORT, this->port);
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-			curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE,
-					(long ) strlen(req) + 1L);
-			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, req);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, res);
+        curl = curl_easy_init ();
+        if ( curl )
+        {
+            curl_easy_setopt (curl, CURLOPT_URL, this->url);
+            curl_easy_setopt (curl, CURLOPT_PORT, this->port);
 
-			r = curl_easy_perform(curl);
-			curl_easy_cleanup(curl);
-		}
-		curl_global_cleanup();
-	}
+            curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt (curl, CURLOPT_POSTFIELDSIZE, (long)strlen (req) + 1L);
+            curl_easy_setopt (curl, CURLOPT_POSTFIELDS, req);
+            curl_easy_setopt (curl, CURLOPT_WRITEDATA, res);
+
+            r = curl_easy_perform (curl);
+            curl_easy_cleanup (curl);
+        }
+        curl_global_cleanup ();
+    }
 }
 
-void HttpExcuteGet(Http* this_gen, char* req, char** res) {
-	HttpExtends* this = (HttpExtends*) this_gen;
-	if (this->conect) {
-		CURL* curl;
-		CURLcode r;
+void HttpExcuteGet (Http this_gen, String req, String * res)
+{
+    HttpExtends * this = (HttpExtends *)this_gen;
+    if ( this->conect )
+    {
+        CURL * curl;
+        CURLcode r;
 
-		curl = curl_easy_init();
-		if (curl) {
-			curl_easy_setopt(curl, CURLOPT_URL, this->url);
-			curl_easy_setopt(curl, CURLOPT_PORT, this->port);
-			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-			curl_easy_setopt(curl, CURLOPT_WRITEDATA, res);
+        curl = curl_easy_init ();
+        if ( curl )
+        {
+            curl_easy_setopt (curl, CURLOPT_URL, this->url);
+            curl_easy_setopt (curl, CURLOPT_PORT, this->port);
+            curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt (curl, CURLOPT_WRITEDATA, res);
 
-			r = curl_easy_perform(curl);
-			curl_easy_cleanup(curl);
-		}
-		curl_global_cleanup();
-	}
+            r = curl_easy_perform (curl);
+            curl_easy_cleanup (curl);
+        }
+        curl_global_cleanup ();
+    }
 }
 
-static size_t write_callback(void* contents, size_t size, size_t nmemb,
-		void** res) {
-	size_t realsize = size * nmemb;
-	*res = (char*) malloc(realsize + 1);
-	strcpy(*res, contents);
-	return realsize;
+static size_t write_callback (void * contents, size_t size, size_t nmemb, void ** res)
+{
+    size_t realsize = size * nmemb;
+    *res = (String)malloc (realsize + 1);
+    strcpy(*res, contents);
+    return realsize;
 }
 
-static size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-	size_t written = fwrite(ptr, size, nmemb, stream);
-	return written;
+static size_t write_data (void * ptr, size_t size, size_t nmemb, FILE * stream)
+{
+    size_t written = fwrite (ptr, size, nmemb, stream);
+    return written;
+}
+
+const char * HttpErrorCheck (CURLcode errorCode)
+{
+    return curl_easy_strerror (errorCode);
 }
