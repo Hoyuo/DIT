4c35dd55372164274f28bbae90563911e26b465c
DongJin <piaf@sogang.ac.kr>
2015-07-25 08:42:52 +0900

    comment modified

--- inc/Commnucation/NFC.h
+++ inc/Commnucation/NFC.h
@@ -23,12 +23,12 @@ void DeleteNDEF (NDEF * ndef);
 
 typedef struct _nfc * NFC;
 
-/**
-	@struct NFC
-	@brief NFC모듈에 대한 구조체이다.
-	@note NFC모듈에 대한 구조체이다. 구조체를 사용하기전에 newNFC() 함수를 사용을 해야 하며 사용이 끝났을 때 deleteNFC()함수를 꼭 사용해야한다.
-	@see https://developer.tizen.org/dev-guide/2.3.0/org.tizen.native.mobile.apireference/group__CAPI__NETWORK__NFC__MODULE.html
- 	@todo features add "http://tizen.org/feature/network.nfc"
+
+/*! @struct NFC
+ *  @brief NFC모듈에 대한 구조체이다.
+ *  @note NFC모듈에 대한 구조체이다. 구조체를 사용하기전에 newNFC() 함수를 사용 해야 하며 사용이 끝났을 때 deleteNFC()함수를 꼭 사용해야한다.
+ *  @see https://developer.tizen.org/dev-guide/2.3.0/org.tizen.native.mobile.apireference/group__CAPI__NETWORK__NFC__MODULE.html
+ *  @todo features에 "http://tizen.org/feature/network.nfc" 를 추가해줘야 한다.
  */
 struct _nfc
 {
@@ -51,13 +51,13 @@ bool onNFCDisconnect (NFC this_gen);
 void NFCSend (NFC this_gen, NDEF message);
 NDEF NFCRecv (NFC this_gen);
 
-/**
-	@struct NFCExtends
-	@brief NFC확장모듈에 대한 구조체이다.
-	@note NFC확모장듈에 대한 구조체이다. 구조체에 NDEF 메세지를 추가해서 사용한다. 메세지를 담당하는 함수들은 내부적으로 추가가 된다.
-	@see https://developer.tizen.org/dev-guide/2.3.0/org.tizen.native.mobile.apireference/group__CAPI__NETWORK__NFC__MODULE.html
- 	@todo features add "http://tizen.org/feature/network.nfc"
+/*!	@struct NFCExtends
+ *  @brief NFC확장모듈에 대한 구조체이다.
+ *  @note NFC확모장듈에 대한 구조체이다. 구조체에 NDEF 메세지를 추가해서 사용한다. 메세지를 담당하는 함수들은 내부적으로 추가가 된다.
+ *  @see https://developer.tizen.org/dev-guide/2.3.0/org.tizen.native.mobile.apireference/group__CAPI__NETWORK__NFC__MODULE.html
+ *  @todo features add "http://tizen.org/feature/network.nfc"
  */
+
 typedef struct
 {
     struct _nfc nfc;
