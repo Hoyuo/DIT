2d0f49e14afb1eade1a0fa4b9fb7ca30f064e07f
zeroise91 <zeroise91@gmail.com>
2015-07-29 15:22:19 +0900

    curl_easy_cleanup() added in http -> disconnect

--- src/Commnucation/Http.c
+++ src/Commnucation/Http.c
@@ -133,6 +133,7 @@ bool onHttpDisconnect (Http this_gen)
             if ( this->url != NULL)
             {
                 free (this->url);
+                curl_easy_cleanup(this->curl);
                 this->url = NULL;
             }
 
